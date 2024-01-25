2ceaf9f73cfdbff6d38a34cccada23009949fade
Trevor <82081705+TrevorMwangi@users.noreply.github.com>
2024-01-25 16:43:40 +0300

    delete test

--- multiclipboard.py
+++ multiclipboard.py
@@ -2,6 +2,12 @@ import sys
 import clipboard
 import json
 
+def save_items(filepath, data):
+    with open(filepath, "w") as f:
+        json.dump(data, f)
+
+data_save = {"key" : "value", "numbers" : [1,2,3]}
+save_items("example.json", data_save)
 
 if len(sys.argv) == 2:
     command = sys.argv[1].lower()
